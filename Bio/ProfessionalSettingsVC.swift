54927f26433ab4e3f291b55bca70a6a6558214a7
Patrick  McDonough <mcdonopj@bc.edu>
2020-09-22 14:44:36 -0400

    pat's commit

--- Bio/ProfessionalSettingsVC.swift
+++ Bio/ProfessionalSettingsVC.swift
@@ -12,7 +12,7 @@ import QuickTableViewController
 
 class ProfessionalSettingsVC: QuickTableViewController {
     
-    @IBOutlet weak var navBarView: NavBarView!
+    var navBarView = NavBarView()
     
     
     //var tabController = NavigationMenuBaseController()
@@ -59,15 +59,29 @@ class ProfessionalSettingsVC: QuickTableViewController {
  var myAccountUserData: [String] = []
  var myAccountArraySupportArrayCombined: [String] = []
  var userDataCombinedArray: [String] = []
-    
+var backButton1 = UIButton()
+var titleLabel1 = UILabel()
     
     override func viewDidLoad() {
-        navBarView.addBehavior()
+//        view.addSubview(navBarView)
+//        navBarView.addSubview(titleLabel1)
+//        navBarView.addSubview(backButton1)
+//        navBarView.addBehavior()
+//        backButton1.setTitle("Back", for: .normal)
+//        backButton1.setTitleColor(.systemBlue, for: .normal)
+//        titleLabel1.text = "Settings"
         super.viewDidLoad()
+        setUpNavBarView()
+//
+//        navBarView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height/12)
+//        tableView.frame = CGRect(x: 0, y: self.view.frame.height/12, width: self.view.frame.width, height: self.view.frame.height)
+//        titleLabel1.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height/12)
+//        backButton1.frame = CGRect(x: 0, y: 0, width: navBarView.frame.width/8, height: titleLabel1.frame.height)
+//        titleLabel1.textAlignment = .center
+//        backButton1.titleLabel?.textAlignment = .left
+//        titleLabel1.font = UIFont(name: "DIN Alternate Bold", size: 40)
+//        navBarView.backgroundColor = .systemGray6
         
-        
-        navBarView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 100)
-        tableView.frame = CGRect(x: 0, y: 100, width: self.view.frame.width, height: self.view.frame.height)
        // navBaView.setTitleVerticalPositionAdjustment(-10, for: UIBarMetrics.default)
 //        backButton.setBackButtonBackgroundVerticalPositionAdjustment(-10, for: UIBarMetrics.default)
         
@@ -103,8 +117,50 @@ class ProfessionalSettingsVC: QuickTableViewController {
         
         
         }
+    
+    func setUpNavBarView() {
+      
+        self.view.addSubview(navBarView)
+        self.navBarView.addSubview(titleLabel1)
+        self.navBarView.addSubview(backButton1)
+        self.navBarView.addBehavior()
+        
+        // tap to dismissSettings
+let dismissTap = UITapGestureRecognizer(target: backButton1, action: #selector(self.backButtonpressed))
+        dismissTap.numberOfTapsRequired = 1
+        self.backButton1.isUserInteractionEnabled = true
+        self.backButton1.addGestureRecognizer(dismissTap)
+
+        
+        self.backButton1.setTitle("Back", for: .normal)
+        self.backButton1.setTitleColor(.systemBlue, for: .normal)
+        self.titleLabel1.text = "Settings"
+        self.navBarView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height/12)
+        self.tableView.frame = CGRect(x: 0, y: self.view.frame.height/12, width: self.view.frame.width, height: self.view.frame.height)
+        self.titleLabel1.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height/12)
+        self.backButton1.frame = CGRect(x: 0, y: 0, width: navBarView.frame.width/8, height: titleLabel1.frame.height)
+        self.titleLabel1.textAlignment = .center
+        self.backButton1.titleLabel?.textAlignment = .left
+        self.titleLabel1.font = UIFont(name: "DIN Alternate Bold", size: 40)
+        self.navBarView.backgroundColor = .systemGray6
+    }
+    
+    @objc func backButtonpressed() {
+        print("It should dismiss here")
+        self.dismiss(animated: true)
+     }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
+        
+                // tap to dismissSettings
+//        let dismissTap = UITapGestureRecognizer(target: backButton1, action: #selector(self.backButtonpressed))
+//                dismissTap.numberOfTapsRequired = 1
+//                self.backButton1.isUserInteractionEnabled = true
+//                self.backButton1.addGestureRecognizer(dismissTap)
+        
+        
+        
 //        menuView.tabController = tabBarController as! NavigationMenuBaseController
 //        menuView.userData = userData
     }
