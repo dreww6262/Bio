594f15127d36d05cf1536ac109717590b18617ab
Drew Williamson <dreww@vt.edu>
2020-10-03 19:59:14 -0400

    zoom for pictures

--- Bio/ContentImageVC.swift
+++ Bio/ContentImageVC.swift
@@ -7,32 +7,38 @@
 
 import UIKit
 
-class ContentImageVC: UIViewController {
+class ContentImageVC: UIViewController, UIScrollViewDelegate {
     
     var photoHex: HexagonStructData?
-    var showOpenAppButton = false 
+    var showOpenAppButton = false
+    var scrollView = UIScrollView()
+    var newImageView: UIImageView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setUpScrollView()
+        setZoomScale()
         
-        let newImageView = UIImageView(image: UIImage(named: "kbit"))
+        
+        newImageView = UIImageView(image: UIImage(named: "kbit"))
         let cleanRef = photoHex!.thumbResource.replacingOccurrences(of: "/", with: "%2F")
         let url = URL(string: "https://firebasestorage.googleapis.com/v0/b/bio-social-media.appspot.com/o/\(cleanRef)?alt=media")
-        newImageView.sd_setImage(with: url!, completed: {_, error, _, _ in
+        newImageView!.sd_setImage(with: url!, completed: {_, error, _, _ in
             if error != nil {
                 print(error!.localizedDescription)
             }
         })
-        self.view.addSubview(newImageView)
+        scrollView.addSubview(newImageView!)
+        scrollView.bringSubviewToFront(newImageView!)
         
         let frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height - 65)
         
-        newImageView.frame = frame
-        newImageView.backgroundColor = .black
+        newImageView!.frame = frame
+        newImageView!.backgroundColor = .black
         
-        newImageView.contentMode = .scaleAspectFit
-        newImageView.isUserInteractionEnabled = true
+        newImageView!.contentMode = .scaleAspectFit
+        newImageView!.isUserInteractionEnabled = true
         //let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
         //newImageView.addGestureRecognizer(tap)
         
@@ -40,6 +46,42 @@ class ContentImageVC: UIViewController {
         textView.text = "asdfkjlasdfjasdf"
         textView.textColor = .red
         // Do any additional setup after loading the view.
+    }
+    
+    // viewdidload helper functions
+    func setUpScrollView() {
+        view.addSubview(scrollView)
+        scrollView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height - 65)
+        scrollView.delegate = self
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.alwaysBounceVertical = false
+        scrollView.alwaysBounceHorizontal = false
+        scrollView.backgroundColor = .black
+        //scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        scrollView.contentMode = .scaleAspectFit
+        scrollView.bouncesZoom = false
+        
+    }
+    
+    func setZoomScale() {
+
+        scrollView.maximumZoomScale = 60
+        scrollView.minimumZoomScale = 1
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let imageViewSize = newImageView!.frame.size
+        let scrollViewSize = scrollView.bounds.size
+        let verticalPadding = imageViewSize.height < scrollViewSize.height ? (scrollViewSize.height - imageViewSize.height)/2 : 0
+        let horizontalPadding = imageViewSize.width < scrollViewSize.width ? (scrollViewSize.width - imageViewSize.width)/2 : 0
+        
+        scrollView.contentInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
+        
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return newImageView
     }
     
 //    @objc func handleTap(_ sender: UITapGestureRecognizer) {
--- Bio/HomeHexagonGrid.swift
+++ Bio/HomeHexagonGrid.swift
@@ -314,15 +314,7 @@ class HomeHexagonGrid: UIViewController, UIScrollViewDelegate, UIGestureRecogniz
     }
     
     
-    func scrollViewDidZoom(_ scrollView: UIScrollView) {
-        let imageViewSize = contentView.frame.size
-        let scrollViewSize = scrollView.bounds.size
-        let verticalPadding = imageViewSize.height < scrollViewSize.height ? (scrollViewSize.height - imageViewSize.height)/2 : 0
-        let horizontalPadding = imageViewSize.width < scrollViewSize.width ? (scrollViewSize.width - imageViewSize.width)/2 : 0
-        
-        scrollView.contentInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
-        
-    }
+    
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         toSearchButton.isHidden = true
