9ec835a64d02500e6a807f1e45d4f1493952f205
Drew Williamson <dreww@vt.edu>
2020-10-03 20:03:17 -0400

    fixed homegrid zoom

--- Bio/HomeHexagonGrid.swift
+++ Bio/HomeHexagonGrid.swift
@@ -146,6 +146,7 @@ class HomeHexagonGrid: UIViewController, UIScrollViewDelegate, UIGestureRecogniz
         contentView.isHidden = false
         scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         resizeScrollView(numPosts: 0)
+        scrollView.bouncesZoom = false
         
     }
     
@@ -330,6 +331,16 @@ class HomeHexagonGrid: UIViewController, UIScrollViewDelegate, UIGestureRecogniz
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return contentView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let imageViewSize = contentView.frame.size
+        let scrollViewSize = scrollView.bounds.size
+        let verticalPadding = imageViewSize.height < scrollViewSize.height ? (scrollViewSize.height - imageViewSize.height)/2 : 0
+        let horizontalPadding = imageViewSize.width < scrollViewSize.width ? (scrollViewSize.width - imageViewSize.width)/2 : 0
+        
+        scrollView.contentInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
+        
     }
     
     func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
